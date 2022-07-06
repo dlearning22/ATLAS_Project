6da414d9e2c4f408875ced4458e0ed831b753f94
dlearning22 <108798218+dlearning22@users.noreply.github.com>
2022-07-06 14:56:41 +0400

    Update loss.py

--- loss.py
+++ loss.py
@@ -26,6 +26,8 @@ class DiceLoss(nn.Module):
 class FocalTverskyLoss(nn.Module):
     def __init__(self, weight=None, size_average=True):
         super(FocalTverskyLoss, self).__init__()
+                self.smooth = sys.float_info.epsilon
+        self.squared_denom = squared_denom
         ALPHA = 0.5
         BETA = 0.5
         GAMMA = 1
