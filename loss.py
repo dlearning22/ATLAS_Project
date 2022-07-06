66271354906863428bc553221259476fbdb57dc5
dlearning22 <108798218+dlearning22@users.noreply.github.com>
2022-07-06 14:13:37 +0400

    Update loss.py

--- loss.py
+++ loss.py
@@ -21,3 +21,30 @@ class DiceLoss(nn.Module):
         denom = x.pow(factor).sum() + target.pow(factor).sum() + self.smooth
         dice_index = numer / denom
         return 1 - dice_index
+
+
+class FocalTverskyLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(FocalTverskyLoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1, alpha=ALPHA, beta=BETA, gamma=GAMMA):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        #inputs = F.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        ALPHA = 0.5
+        BETA = 0.5
+        GAMMA = 1
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        #True Positives, False Positives & False Negatives
+        TP = (inputs * targets).sum() #intesection   
+        FP = ((1-targets) * inputs).sum()
+        FN = (targets * (1-inputs)).sum()
+        
+        Tversky = (TP + smooth) / (TP + alpha*FP + beta*FN + smooth)  
+        FocalTversky = (1 - Tversky)**gamma
+                       
+        return FocalTversky
